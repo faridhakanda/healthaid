37f62a695f670010c15d8031bc6ad02559ab5094
farid-akanda <faridakanda1729@gmail.com>
2024-03-19 11:56:54 +0600

    zakaria

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className="top-10 pt-16 text-center bg-indigo-300">
-        <h1>Abdul Alim</h1>
-        <p>&</p>
-        <h1>Zakaria Ahmed</h1>
+    <div className="top-10 pt-16 text-center bg-indigo-300 pb-2">
+        <div className="bg-sky-300 w-48 h-fit rounded justify-center text-center mx-auto p-2">
+            <h1>Abdul Alim</h1>
+            <p>&</p>
+            <h1>Zakaria Ahmed</h1>
+        </div>
+        
     </div>
   )
 }
