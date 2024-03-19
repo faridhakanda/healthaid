df31657037bc832e497f3d92beb55912011a2c5c
farid-akanda <faridakanda1729@gmail.com>
2024-03-19 11:53:50 +0600

    alim and zakaria

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="top-10 pt-16 text-center bg-indigo-300">
+        <h1>Abdul Alim</h1>
+        <p>&</p>
+        <h1>Zakaria Ahmed</h1>
+    </div>
+  )
+}
+
+export default About
