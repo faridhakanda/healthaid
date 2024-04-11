dfc5366f7540c819146e111f493a68daf6529636
farid-akanda <faridakanda1729@gmail.com>
2024-04-11 11:08:53 +0600

    for about part

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,9 +4,7 @@ const About = () => {
   return (
     <div className="top-10 pt-16 text-center bg-indigo-300 pb-2">
         <div className="bg-sky-300 w-48 h-fit rounded justify-center text-center mx-auto p-2">
-            <h1>Abdul Alim</h1>
-            <p>&</p>
-            <h1>Zakaria Ahmed</h1>
+            <h1>This is page for about part in my web application!</h1>
         </div>
         
     </div>
